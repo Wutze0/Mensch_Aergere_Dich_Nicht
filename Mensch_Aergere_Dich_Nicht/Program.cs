b48357d5fd185b397f41737f28fa9207b0b47e1c
Julian <165800544+Wutze0@users.noreply.github.com>
2024-04-18 11:29:33 +0200

    Beginn SPielfeldausgabe

--- Mensch_Aergere_Dich_Nicht/Program.cs
+++ Mensch_Aergere_Dich_Nicht/Program.cs
@@ -4,52 +4,111 @@ namespace Mensch_Aergere_Dich_Nicht
     {
         static void Main(string[] args)
         {
-            
-            Haus blauesHaus = new Haus("blau");
-            Haus gruenesHaus = new Haus("gruen");
-            Haus gelbesHaus = new Haus("gelb");
-            Haus rotesHaus = new Haus("rot");
+
+            Haus blauesHaus = new Haus(Verfuegbare_Farben.Blau);
+            Haus gruenesHaus = new Haus(Verfuegbare_Farben.Gruen);
+            Haus gelbesHaus = new Haus(Verfuegbare_Farben.Gelb);
+            Haus rotesHaus = new Haus(Verfuegbare_Farben.Rot);
             List<Haus> haueser = new List<Haus>();
             haueser.Add(blauesHaus);
             haueser.Add(gruenesHaus);
             haueser.Add(gelbesHaus);
             haueser.Add(rotesHaus);
 
+            haueser.ElementAt(1).ZugehoerigeFiguren.ElementAt(2).PrintPosition = 3;
             PrintSpielfeld(haueser);
-            
+
         }
 
         static void PrintSpielfeld(List<Haus> haueser)
         {
             string zeilestandard = "\t\t\t\t\t#    |    |    #\t\t";
+            string zeileMitSpieler = "\t\t\t\t\t  ";
+
+            List<Spielfigur> figurenInZeile = new List<Spielfigur>();
 
             int x = 50;
+            int j;
+
             string rahmen = "\t\t----------------------------------------------------------------";                 // ein \t entspricht 8 "-" Zeichen
             Console.WriteLine(rahmen);
             Console.WriteLine("\t\t\t\t\t################\t\t");
 
-            for (int i = 0; i <= 4;  i++)
+            for (int i = 0; i <= 4; i++)
             {
-                if(i % 2 == 0)
+                if (i % 2 == 0)
                 {
-                    foreach(Haus h in haueser)
+                    if (i == 2)
                     {
-                        foreach(Spielfigur s in h._zugehoerigeFiguren)
+
+                        foreach (Haus h in haueser)
                         {
-                            if (Spielfigur.) 
+                            foreach (Spielfigur s in h.ZugehoerigeFiguren)
+                            {
+                                if (s.PrintPosition == 1 || s.PrintPosition == 2 || s.PrintPosition == 3)
+                                {
+                                    figurenInZeile.Add(s);
+                                }
+                            }
                         }
+                        int printcounter = 0;
+                        int check;
+                        Console.Write(zeileMitSpieler);
+
+                        check = figurenInZeile.FindIndex(i => i.PrintPosition == 1);
+                        if (check == -1)
+                        {
+                            Console.Write("     ");
+                        }
+                        else
+                        {
+                            figurenInZeile.ElementAt(check).PrintFigur();
+                            Console.Write("   ");
+                        }
+
+
+                        check = figurenInZeile.FindIndex(i => i.PrintPosition == 2);
+                        if (check == -1)
+                        {
+                            Console.Write("     ");
+                        }
+                        else
+                        {
+                            figurenInZeile.ElementAt(check).PrintFigur();
+                            Console.Write("   ");
+                        }
+
+
+
+                        check = figurenInZeile.FindIndex(i => i.PrintPosition == 3);
+                        if (check == -1)
+                        {
+                            Console.Write("     ");
+                        }
+                        else
+                        {
+                            figurenInZeile.ElementAt(check).PrintFigur();
+                            Console.Write("   ");
+                        }
+
+
+                        Console.Write('\n');
                     }
-                    Console.WriteLine("");
+                    else
+                    {
+                        Console.WriteLine("");
+                    }
+
                 }
-                else if(i % 2 == 1)
+                else if (i % 2 == 1)
                 {
-                     Console.WriteLine(zeilestandard);
+                    Console.WriteLine(zeilestandard);
                 }
             }
 
 
 
-            
+
 
 
 
--- Mensch_Aergere_Dich_Nicht/Spielfigur.cs
+++ Mensch_Aergere_Dich_Nicht/Spielfigur.cs
@@ -2,16 +2,29 @@
 {
     internal class Spielfigur
     {
-        private int _id;
+        public int _id { get; set; }
         private string _farbe;
         private double _position;
-        private double _printPosition { get; set; } = 0;
+        public double PrintPosition { get; set; } = 0;
 
         public Spielfigur(int id, string farbe, double position)
         {
             _id = id;
             _farbe = farbe;
             _position = position;
+        }
+
+        public void PrintFigur()
+        {
+            switch (_farbe)
+            {
+                case "Rot": Console.ForegroundColor = ConsoleColor.Red; Console.Write($"P{_id}"); break;
+                case "Gruen": Console.ForegroundColor = ConsoleColor.Green; Console.Write($"P{_id}"); break;
+                case "Blau": Console.ForegroundColor = ConsoleColor.Blue; Console.Write($"P{_id}"); break;
+                case "Gelb": Console.ForegroundColor = ConsoleColor.Yellow; Console.Write($"P{_id}"); break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
