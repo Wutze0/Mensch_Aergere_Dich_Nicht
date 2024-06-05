0a4a3bba01caccb71056760c4446742b04a130d6
Julian <165800544+Wutze0@users.noreply.github.com>
2024-06-05 11:43:00 +0200

    Benutzererstellung angefangen

---
+++ Mensch_Aergere_Dich_Nicht/AngemeldeterSpieler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensch_Aergere_Dich_Nicht
+{
+    internal class AngemeldeterSpieler : Menschlicher_Spieler
+    {
+        private string _email;
+        private string _password;
+        public int _wins { get; set; }
+        public AngemeldeterSpieler(string name, string passwort, string email) : base(name)
+        {
+            _email = email;
+            _password = passwort;
+        }
+    }
+}
