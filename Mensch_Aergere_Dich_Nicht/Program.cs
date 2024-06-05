d8333349b875def200bb22b27ebb45f1091d1457
Samuel <131021077+Cannonball1234@users.noreply.github.com>
2024-06-05 12:22:30 +0200

    Anfang Anmeldung (noch nicht fertig)

--- Mensch_Aergere_Dich_Nicht/Program.cs
+++ Mensch_Aergere_Dich_Nicht/Program.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
 using System.Text.RegularExpressions;
 
 namespace Mensch_Aergere_Dich_Nicht
@@ -476,7 +477,7 @@ namespace Mensch_Aergere_Dich_Nicht
 
                     }
                 }
-                catch (Exception)
+                catch (UserFalscheEingabeException)
                 {
                     Console.WriteLine("Falsche Eingabe... erneuter Versuch:");
                 }
@@ -561,7 +562,7 @@ namespace Mensch_Aergere_Dich_Nicht
                 if(fsR.Length == 0)
                 {
                     fsR.Close();
-                    FileStream fsW = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+                    FileStream fsW = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
                     StreamWriter sw = new StreamWriter(fsW);
                     sw.WriteLine("Name\tEmail\tPasswort");
                     sw.Close();
@@ -574,7 +575,34 @@ namespace Mensch_Aergere_Dich_Nicht
 
                 switch (auswahl)
                 {
-                    case 1: break;
+                    case 1:
+                        Console.WriteLine("Bitte geben Sie Ihren Nutzernamen ein:");
+                        string eingabe = Console.ReadLine();
+                        string dir = Directory.GetCurrentDirectory();
+                        string p = dir + @"/Spielerdaten";
+
+                        FileStream fss = new FileStream(p + "/Spielerdaten.txt", FileMode.OpenOrCreate, FileAccess.Read);
+                        StreamReader srr = new StreamReader(fss);
+
+                        string inhlt = srr.ReadToEnd();
+                        string[] inhlt2 = inhlt.Split('\t');
+                        for(int a = 1; a < inhlt2.Length; a++)
+                        {
+                            if (inhlt2[a-1].StartsWith(eingabe) || inhlt2[a - 1].StartsWith(eingabe))
+                            {
+                                Console.WriteLine($"Bitte geben Sie das Passwort für {eingabe} ein!");
+                                string pw = Passsworteinlesen();
+                                if(pw.Equals(inhlt2[a-1].Split('\t')[2]))
+                                {
+                                    Console.WriteLine("Sie haben sich erfolgreich angemeldet!");
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        break;
                     case 2:
                         string passwort;
                         string passwort2;
